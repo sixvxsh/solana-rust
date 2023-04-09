93a059b24e9c76b59ac2dab67f030d09af6d4701
sixvxsh <sixvxsh.j@gmail.com>
2023-04-09 13:15:50 +0330

    variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!, i'm Siavash");
+    let unsigned: u8 = 10;
+    let signed: i8 = -100;
+    let float: f32 = 1.0;
+    println!("unsign: {} sign: {} float: {}", unsigned, signed, float);
+
+    let _letter= "c1234";
+    let _emoji = "\u{1f600}";
+    println!("letter: {}, emoji: {}", _letter, _emoji);
+
+    let is_true: bool = true;
+    println!("isTrue:{}", is_true);
 }
